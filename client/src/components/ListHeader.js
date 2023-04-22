cb508ce1e169fcb534527c9fbdd6e4d3c6c339e4
Sergi <scsanz87@gmail.com>
2023-04-22 14:11:18 +0200

    PostgreSQL + btns styled

--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,7 +1,17 @@
 const ListHeader = ({ listName }) => {
+
+    const salir = () => {
+      console.log("has salido");
+
+    }
+
     return (
       <div className = "list-header">
         <h1>{listName}</h1>
+        <div className="btn-container">
+          <button className="añadir">ADD NEW</button>
+          <button className="salir" onClick={salir}>SIGN OUT</button>
+        </div>
       </div>
     );
   }
